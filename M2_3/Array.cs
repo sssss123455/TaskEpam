594493e8d69b0f9942a04c378029597ccc2f06d4
Олег Шильников <shilnikovandoleg@gmail.com>
2021-04-07 17:22:59 +0600

    add M2_3

---
+++ M2_3/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M2_3
+{
+    public class Array
+    {
+        public int GetNumber(double[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                var startNumberForsumBeforeNumber = 0;
+                var endNumberForsumBeforeNumber = i - 1;
+                var startNumberForsumAfterNumber = i + 1;
+                var endNumberForsumAfterNumber = array.Length - 1;
+                var sumBeforeNumber = Sum(array, startNumberForsumBeforeNumber, endNumberForsumBeforeNumber);
+                var sumAfterNumber = Sum(array, startNumberForsumAfterNumber, endNumberForsumAfterNumber);
+                if (sumBeforeNumber == sumAfterNumber)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        private static double Sum(double[] array, int startNumber, int endNumber)
+        {
+            double sum = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ M2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace M2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            var example = array.GetNumber(new double[] { 1, 2, 3.4, 5, 1, 2.4, 3 });
+            Console.WriteLine(example);
+        }
+    }
+}
