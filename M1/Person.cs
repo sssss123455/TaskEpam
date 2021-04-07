26cbbcbbdd21a9446e3fcb4cf32f669ab6576c8a
Олег Шильников <shilnikovandoleg@gmail.com>
2021-04-07 15:46:28 +0600

    Добавьте файлы проекта.

---
+++ M1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M1
+{
+    class Person
+    {
+        public void GetMessage()
+        {
+            Console.WriteLine("man");
+        }
+    }
+}
---
+++ M1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine("Hi");
+            person.GetMessage();
+        }
+    }
+}
