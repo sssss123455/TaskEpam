88ec3ebd0b855fc858a67c3f6ae9858427de54cd
Олег Шильников <shilnikovandoleg@gmail.com>
2021-04-07 18:14:10 +0600

    add M2_4

---
+++ M2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.GetString("asd","asdff"));
+        }
+    }
+}
---
+++ M2_4/String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M2_4
+{
+    public class String
+    {
+        public static string GetString(string stringFirst, string stringSecond)
+        {
+            string resultingString = null;
+           
+            foreach (var item in stringFirst.ToList().Concat(stringSecond).ToList().Distinct().ToList())
+            {
+               resultingString+=item;
+            }
+            
+            return resultingString;
+        }
+    }
+}
